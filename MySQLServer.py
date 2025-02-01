5269ad3d2d5a94ca81eef3e77fba51c12585d96e
LARRY <tingzlarry@gmail.com>
2025-02-01 12:29:39 +0300

    Initial commit: Added Python script to create MySQL database

--- MySQLServer.py
+++ MySQLServer.py
@@ -4,23 +4,23 @@ from mysql.connector import Error
 def create_database():
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            user='larry',
-            password='12345'
+            host="localhost",
+            user="larry",
+            password="1234"
         )
-
         if connection.is_connected():
             cursor = connection.cursor()
-            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            create_db_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"
+            cursor.execute(create_db_query)
             print("Database 'alx_book_store' created successfully!")
-
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
-
     finally:
         if 'connection' in locals() and connection.is_connected():
             cursor.close()
             connection.close()
+            print("MySQL connection is closed.")
+
 
 # Ensure the function runs only when the script is executed directly
 if __name__ == "__main__":
