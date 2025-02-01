f83eddabefaf86522c2e6abe3fadc667e8d92515
LARRY <tingzlarry@gmail.com>
2025-02-01 12:59:01 +0300

    Adding task 4 a full description

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,4 @@
+--a script that prints the full description of the table books from the database alx_book_store in your MySQL server--
 USE alx_book_store;
 
 SELECT COLUMN_NAME, COLUMN_TYPE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA, IS_NULLABLE 
